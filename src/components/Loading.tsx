33275290c48f7fe693113466241629a2215664e6
RoshBJr <roshanjr3@gmail.com>
2023-02-17 22:29:37 -0500

    modified loading screen

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -7,8 +7,15 @@ export default function Loading() {
     
     return (
         <div className="loading-container">
+            {/* <img className='logo' src={loadingGif} alt="Loading..." /> */}
+            <div className="container-logo">
+                <div className="top-bar"></div>
+                <div className="left-bar"></div>
+                <div className='logo'>R</div>
+                <div className="right-bar"></div>
+                <div className="bottom-bar"></div>
+            </div>
             <div className="hide-gif"></div>
-            <img className='logo' src={loadingGif} alt="Loading..." />
         </div>
     );
 }
