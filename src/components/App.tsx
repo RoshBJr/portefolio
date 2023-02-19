43481b8a39f1d4fe087c91764e50733f69ec9329
RoshBJr <roshanjr3@gmail.com>
2023-02-18 20:24:17 -0500

    modified css for loading screen

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -16,9 +16,7 @@ function App() {
 
   return (
     <div className="App">
-      {/* {loading ? (<Loading/>) : (<Main/>)} */}
-      <Header/>
-      <Main/>
+      {loading ? (<Loading/>) :(<Main/>)}
     </div>
   );
 }
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,13 +1,17 @@
+import Header from './Header';
 import './Main.scss';
 import Presentation from './Presentation';
 
 export default function Main() {
 
     return (
+        <>
+        <Header/>
         <div className="main">
             <section className="all-sections">
                 <Presentation/>
             </section>
         </div>
+        </>
     );
 }
