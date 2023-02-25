9d47884e8432c617698228cf3c4403e5eb38deb7
RoshBJr <roshanjr3@gmail.com>
2023-02-24 22:54:05 -0500

    section competence langages done x-small screen

--- src/components/Competence.tsx
+++ src/components/Competence.tsx
@@ -2,33 +2,48 @@ import './Competence.scss';
 import data from '../data/data-fr.json';
 import TitreSection from './UI/TitreSection';
 import * as Icons from './UI/Icons';
+import LigneCompetence from './UI/LigneCompetence';
 
 export default function Competence() {
 
     return (
         <div className="container-competence">
-            <TitreSection titre={data['section-competence'].titre} />
-            <div className="container-titre-img">
-                <h2 className="titre">{data['section-competence']['section-langages'].js.titre}</h2>
-                <div className='container-icon'></div>
-                <Icons.PrIcon/>
-                <Icons.AiIcon/>
-                <Icons.AndStudioIcon/> {/*not working*/}
-                <Icons.CSharpIcon/>
-                <Icons.CssIcon/>
-                <Icons.FigmaIcon/>
-                <Icons.GitIcon/>
-                <Icons.GitHubIcon/>
-                <Icons.HtmlIcon/>
-                <Icons.KotlinIcon/>
-                <Icons.PhpIcon/>
-                <Icons.PrIcon/>
-                <Icons.PsIcon/>
-                <Icons.ReactIcon/>
-                <Icons.SassIcon/>
-                <Icons.TsIcon/>
-                <Icons.XdIcon/>
-                <Icons.XmlIcon/>
+                <TitreSection titre={data['section-competence'].titre} />
+            <div className="boite-langages">
+                <h2 className="boite-titre">
+                    {data['section-competence']['section-langages'].titre}
+                </h2>
+
+                <LigneCompetence Icone={Icons.JsIcon}
+                    titre={data['section-competence']['section-langages'].js.titre} />
+
+                <LigneCompetence Icone={Icons.TsIcon} 
+                    titre={data['section-competence']['section-langages'].ts.titre} />
+                    
+                <LigneCompetence Icone={Icons.CssIcon} 
+                    titre={data['section-competence']['section-langages'].css.titre} />
+
+                <LigneCompetence Icone={Icons.SassIcon} 
+                    titre={data['section-competence']['section-langages'].sass.titre} />
+
+                <LigneCompetence Icone={Icons.HtmlIcon} 
+                    titre={data['section-competence']['section-langages'].html.titre} />
+
+                <LigneCompetence Icone={Icons.CSharpIcon} 
+                    titre={data['section-competence']['section-langages'].csharp.titre} />
+
+                <LigneCompetence Icone={Icons.PhpIcon} 
+                    titre={data['section-competence']['section-langages'].php.titre} />
+
+                <LigneCompetence Icone={Icons.KotlinIcon} 
+                    titre={data['section-competence']['section-langages'].kotlin.titre} />
+
+                <LigneCompetence Icone={Icons.XmlIcon} 
+                    titre={data['section-competence']['section-langages'].xml.titre} />
+
+                <LigneCompetence Icone={Icons.GitIcon} 
+                    titre={data['section-competence']['section-langages'].git.titre} />
+
             </div>
         </div>
     );
---
+++ src/components/UI/LigneCompetence.tsx
@@ -0,0 +1,18 @@
+import { JsIcon } from "./Icons";
+import data from '../../data/data-fr.json';
+import '../UI/LigneCompetence.scss';
+
+interface Props {
+    Icone:React.FC;
+    titre:string;
+}
+
+export default function LigneCompetence({Icone, titre}:Props) {
+
+    return (
+        <div className="container-une-lang">
+            <div className="icone"><Icone/></div>
+            <h2 className="titre">{titre}</h2>
+        </div>
+    );
+}
