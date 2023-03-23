aa9251942a2dc4f691b9edad936b6a6d0869880d
RoshBJr <roshanjr3@gmail.com>
2023-03-22 18:29:34 -0600

    ajout de projets

--- src/components/Projet.tsx
+++ src/components/Projet.tsx
@@ -2,6 +2,7 @@ import './Projet.scss';
 import BoiteProjet from './UI/BoiteProjet';
 import data from '../data/data-fr.json';
 import TitreSection from './UI/TitreSection';
+import * as Icons from "./UI/Icons";
 import { useRef, useState, useEffect } from 'react';
 
 export default function Projet() {
@@ -26,7 +27,26 @@ export default function Projet() {
         <div className={`container-projet ${ isVisible ? 'is-visible': ''}`}>
             <div id='projets' ></div>
             <TitreSection titre={data['section-projets'].titre} />
-            <BoiteProjet/>
+            <BoiteProjet
+              titre={data['section-projets'].projet1.titre}
+              desc={data['section-projets'].projet1.desc}
+              logos={[<Icons.CSharpIcon/>, <Icons.UnityIcon/>]}
+              img={data['section-projets'].projet1.img}
+            />
+
+            <BoiteProjet
+              titre={data['section-projets'].projet2.titre}
+              desc={data['section-projets'].projet2.desc}
+              logos={[<Icons.AndStudioIcon/>, <Icons.KotlinIcon/>, <Icons.XmlIcon/>]}
+              img={data['section-projets'].projet2.img}
+            />
+
+            <BoiteProjet
+              titre={data['section-projets'].projet3.titre}
+              desc={data['section-projets'].projet3.desc}
+              logos={[<Icons.AndStudioIcon/>, <Icons.KotlinIcon/>, <Icons.XmlIcon/>]}
+              img={data['section-projets'].projet3.img}
+            />
             <div ref={domRef} className="trigger-anim-titre-projet"></div>            
         </div>
     );
--- src/components/UI/BoiteProjet.tsx
+++ src/components/UI/BoiteProjet.tsx
@@ -3,8 +3,14 @@ import data from '../../data/data-fr.json';
 import * as Icons from "../UI/Icons";
 import { useRef, useState, useEffect } from 'react';
 
-export default function BoiteProjet() {
+interface UIProps {
+    titre: string;
+    desc: string;
+    logos: any[];
+    img: string;
+}
 
+export default function BoiteProjet({titre, desc, logos, img}:UIProps) {
 
     const [isVisible, setVisible] = useState(false);
 
@@ -25,20 +31,19 @@ export default function BoiteProjet() {
         <div className={`boite-projet ${ isVisible ? 'is-visible': ''}`}>
             
             <div className="container-info">
-                <h2 className="titre">{data['section-projets'].projet1.titre}</h2>
-                <p className="desc">{data['section-projets'].projet1.desc}</p>
+                <h2 className="titre">{titre}</h2>
+                <p className="desc">{desc}</p>
                 
                 <div className="logiciels">
                     <h3 className="sous-titre">Réalisé avec</h3>
-                    <div className="logo"><Icons.CSharpIcon/></div>
-                    <div className="logo"><Icons.UnityIcon/></div>
+                    {logos.map( logo => <div className="logo">{logo}</div>)}
                 </div>
                 <div className="lien">
                     <a className="logo github">{<Icons.GitHubIcon/>}</a>
                     <a className=" logo projet">{<Icons.BuildIcon/>}</a>
                 </div>
             </div>
-            <img src={require('../media/lfo.png')} alt="" />
+            <img src={require(`../media/${img}.png`)} alt="" />
             <div ref={domRef} className="trigger-anim-fwrk"></div>
         </div>
     );
